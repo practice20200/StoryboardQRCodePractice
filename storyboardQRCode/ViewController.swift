7193840cbd8471931bb9b3467131cd01f8b2daf6
practice20200 <wor9hard2018@gmail.com>
2022-06-20 14:17:49 -0700

    initial commit

---
+++ storyboardQRCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  storyboardQRCode
+//
+//  Created by Apple New on 2022-06-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
